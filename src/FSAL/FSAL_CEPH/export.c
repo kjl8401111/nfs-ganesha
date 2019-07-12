c534a46f5f170002add782494939c4a57194f8f0
kjl8401111 <kjl8401@126.com>
2019-07-12 10:34:14 +0800

    Update export.c
    realpath not get the first address to appear of fullpath from if

--- src/FSAL/FSAL_CEPH/export.c
+++ src/FSAL/FSAL_CEPH/export.c
@@ -138,7 +138,8 @@ static fsal_status_t lookup_path(struct fsal_export *export_pub,
 	 * must be a superset of the export fullpath, or the string
 	 * handling will be broken.
 	 */
-	if (strstr(realpath, op_ctx->ctx_export->fullpath) != realpath) {
+	realpath = strstr(realpath, op_ctx->ctx_export->fullpath);
+	if (realpath == NULL) {
 		status.major = ERR_FSAL_SERVERFAULT;
 		return status;
 	}
